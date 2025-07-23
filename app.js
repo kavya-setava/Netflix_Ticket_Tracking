290414248c3f5aa676a2cce0343de376fccd089c
kunapureddysaiteja <saiteja.kunapureddy@mediamint.com>
2025-07-23 15:18:15 +0530

    c

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors'); 
+const app = express();
+const connectDB = require('./DataBase/db');
+//const route = require('./routes/indexRoute');
+
+dotenv.config();
+connectDB();
+
+
+app.use(cors({
+  origin: ['http://localhost:4200','http://localhost:5173'],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true 
+}));
+
+app.use(express.json());
+//app.use('/api', route);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
